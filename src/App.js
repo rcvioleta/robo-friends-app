4bd4ee13816cc9df9ef3675d75dfb568c64deaee
Rogene Cris Violeta <rcvioleta13@gmail.com>
2019-05-02 18:05:06 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = props => {
+  const [fetchedRobots, setFetchedRobots] = useState(null);
+  const [searchField, setSearchField] = useState(null)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resp => resp.json())
+    .then(data => setFetchedRobots(data))
+    .catch(err => console.log(err))
+  }, []);
+
+  // useEffect(() => {
+  //   return () => {
+  //     console.log('searching...', searchField);
+  //   }
+  // }, [searchField]);
+
+  const searchHandler = event => {
+    console.log('event triggered', event.target.value);
+    setSearchField(event.target.value);
+  }
+  
+  let robots = null
+
+  if (fetchedRobots && !searchField) {
+    robots = fetchedRobots.map(robot => (
+      <div key={robot.id}>
+        <h3>{robot.name}</h3>
+        <p>{robot.email}</p>
+        <p>{robot.phone}</p>
+      </div>  
+    ));
+  } else if (searchField) {
+    const filteredRobots = fetchedRobots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+    robots = filteredRobots.map(robot => (
+      <div key={robot.id}>
+        <h3>{robot.name}</h3>
+        <p>{robot.email}</p>
+        <p>{robot.phone}</p>
+      </div>  
+    ));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>RoboFriendsApp</h1>
+        <input type="text" placeholder="Search your robo friend" onChange={searchHandler}/>
       </header>
+      <main>
+        { robots }
+      </main>
     </div>
   );
 }
